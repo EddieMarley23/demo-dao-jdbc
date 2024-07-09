bc527f87e1427bfad633e926fc26bf7cc259eff0
Eddie Marley <marley_eddie@hotmail.com>
2024-07-09 19:49:30 -0300

    Reusing instantiation

--- src/application/ProgramSC21.java
+++ src/application/ProgramSC21.java
@@ -14,6 +14,8 @@ public class ProgramSC21 {
 	
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("==== TEST 1: Seller findById");
 		Seller seller = sellerDao.findById(3); 
 		
 		System.out.println(seller);
